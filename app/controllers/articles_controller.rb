05e75196ac3f0ca0a154ae5e447ed7438341c991
akumarm23 <ak.muniswamy2023@gmail.com>
2025-01-15 15:16:57 -0500

    Cleaned up Code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -11,14 +12,42 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Awesome! New Article Created :)"
       redirect_to @article 
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+  
+    if @article.update(article_params)
+      flash[:notice] = "Updated Article :)"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
